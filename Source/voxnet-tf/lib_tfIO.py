e757e151e667ef1334f084f9aa67386c0c981554
Tobi Grundmann <tobiagru@users.noreply.github.com>
2016-04-04 12:58:25 +0200

    Update lib_tfIO.py
    Added load and format for Theano

--- Source/voxnet-tf/lib_tfIO.py
+++ Source/voxnet-tf/lib_tfIO.py
@@ -6,8 +6,33 @@ from sklearn.utils import shuffle
 
 import voxnet_tf_cfg
 
+#load and format for Theano/keras
+def load_and_format_th((fileTar, cfg, load_tst_chunk = 0):
+    #open TarFile
+    reader = npytar.NpyTarReader(fileTar)
+    #if wanted load only a chunk to enable testing
+    if load_tst_chunk is not 0:
+        n_observations = load_tst_chunk
+    else:
+        n_observations = reader.elements()
+    #theano format is (#observations x #channels x imgsize)
+    tf_format = (n_observations,) + (cfg['n_channels'] + cfg['dims'],)
+    #create features & labels matrix
+    dataset = np.zeros(tf_format, dtype=np.float32)
+    labels = []
+    #iterate through all matrixes in tarfile and save as tensorflow format
+    for it, (x, name) in enumerate(reader):
+        dataset[it] = x.astype(np.float32)
+        labels.append(int(name.split('.')[0])-1)
+        if len(labels) >= n_observations:
+            break
+    #binaritize labels
+    labels = (np.arange(cfg['n_classes']) == labels[:,None]).astype(np.float32)
+    #return features & labels
+    return dataset, labels
+
 #load data from Tar-file and format into tensorflow format
-def load_and_format(fileTar, cfg, load_tst_chunk = 0):
+def load_and_format_tf(fileTar, cfg, load_tst_chunk = 0):
     #open TarFile
     reader = npytar.NpyTarReader(fileTar)
     #if wanted load only a chunk to enable testing
