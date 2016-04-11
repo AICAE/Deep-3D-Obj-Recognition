8be5bd8486bec4f11d9e9f68a07d7a3d787e8ba8
tobiagru <tobiagru@student.ethz.ch>
2016-04-11 19:37:25 +0200

    deleted lib_tfIO

--- Source/voxnet-tf/lib_tfIO.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-sys.path.insert(0, os.path.dirname(os.path.realpath(__file__) + '/config/'))
-import voxnet_cfg
-import modelnet_cfg
-
-def print_dir():
-    print(os.path.dirname(os.path.realpath(__file__) + '/config/'))
