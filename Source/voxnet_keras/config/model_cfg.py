244424eca719ba4bd6e51a0a0f71f4289d805613
tobiagru <tobiagru@student.ethz.ch>
2016-04-12 08:43:57 +0200

    changed IO

--- Source/voxnet_keras/config/model_cfg.py
+++ Source/voxnet_keras/config/model_cfg.py
@@ -1,4 +1,4 @@
-class_id_to_name = {
+class_id_to_name_modelnet10 = {
     "1": "bathtub",
     "2": "bed",
     "3": "chair",
@@ -10,8 +10,55 @@ class_id_to_name = {
     "9": "table",
     "10": "toilet"
 }
-class_name_to_id = { v : k for k, v in class_id_to_name.items() }
-class_names = set(class_id_to_name.values())
+class_name_to_id_modelnet10 = { v : k for k, v in class_id_to_name_modelnet10.items() }
+class_names_modelnet10 = set(class_id_to_name_modelnet10.values())
+
+
+class_id_to_name_modelnet40 = {
+'1':'airplane',
+'2':'bookshelf',
+'3':'chair',
+'4':'desk',
+'5':'glass_box',
+'6':'laptop',
+'7':'person',
+'8':'range_hood',
+'9':'stool',
+'10':'tv_stand',
+'11':'bathtub',
+'12':'bottle',
+'13':'cone',
+'14':'door',
+'15':'guitar',
+'16':'mantel',
+'17':'piano',
+'18':'sink',
+'19':'table',
+'20':'vase',
+'21':'bed',
+'22':'bowl',
+'23':'cup',
+'24':'dresser',
+'25':'keyboard',
+'26':'monitor',
+'27':'plant',
+'28':'sofa',
+'29':'tent',
+'30':'wardrobe',
+'31':'bench',
+'32':'car',
+'33':'curtain',
+'34':'flower_pot',
+'35':'lamp',
+'36':'night_stand',
+'37':'radio',
+'38':'stairs',
+'39':'toilet',
+'40':'xbox',
+}
+class_name_to_id_modelnet40 = { v : k for k, v in class_id_to_name_modelnet40.items() }
+class_names_modelnet40 = set(class_id_to_name_modelnet40.values())
+
 
 lr_schedule = { 0: 0.001,
                 60000: 0.0001,
--- Test/test_IO.py
+++ Test/test_IO.py
@@ -1,21 +1,30 @@
 import numpy as np
-from matplotlib import pyplot as plt
-
+# #import mayavi.mlab as mlab
+#
 from Source.voxnet_keras import lib_IO
 from Source.voxnet_keras.config import model_cfg
 
-fname_testset = '/home/tg/Projects/Deep-3D-Obj-Recognition/Source/Data/modelnet10_test.tar'
-fname_trainset = '/home/tg/Projects/Deep-3D-Obj-Recognition/Source/Data/modelnet10_train.tar'
+##############test-saving####################################################
+print(model_cfg.class_names_modelnet10)
 
-cfg = model_cfg.voxnet_cfg
+lib_IO.save_dataset_as_npy("/home/tg/Downloads/volumetric_data/",
+                           "/home/tg/Projects/Deep-3d-Obj-Recognition/Source/Data/modelnet10.npz",
+                           model_cfg.class_name_to_id_modelnet10)
 
-dataset, labels = lib_IO.load_and_format(fname_testset, cfg)
 
-print('dataset shape is {1}   -- should be .. x 1 x 32 x 32 x 32'.format(dataset.shape))
-print('dataset shape is {1}   -- should be .. x 2'.format(labels.shape))
-
-firstimg = dataset[np.random.randint(10,100),1,:,:,:]
-print(firstimg.shape)
-
-fig = plt.figure()
+##############test-loading###################################################
+# fname_testset = '/home/tg/Projects/Deep-3D-Obj-Recognition/Source/Data/modelnet10_test.tar'
+# fname_trainset = '/home/tg/Projects/Deep-3D-Obj-Recognition/Source/Data/modelnet10_train.tar'
+#
+# cfg = model_cfg.voxnet_cfg
+#
+# dataset, labels = lib_IO.load_and_format(fname_testset, cfg)
+#
+# print('dataset shape is {1}   -- should be .. x 1 x 32 x 32 x 32'.format(dataset.shape))
+# print('dataset shape is {1}   -- should be .. x 2'.format(labels.shape))
+#
+# firstimg = dataset[np.random.randint(10,100),0,:,:,:]
+# print(firstimg.shape)
+#
+# #fig = plt.figure()
 
