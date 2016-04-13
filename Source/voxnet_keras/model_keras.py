b72a12899b1b2c4c65ac4a378a7b4cbe195e842c
Johannes <voswaldj@student.ethz.ch>
2016-04-13 21:10:20 +0200

    model_keras first working version
    viele work arounds, funktioniert das alles so auch? Inhaltlich prüfen!

--- Source/voxnet_keras/model_keras.py
+++ Source/voxnet_keras/model_keras.py
@@ -5,7 +5,7 @@ from keras import backend as K
 from keras.models import Sequential
 
 from keras.layers import Convolution3D, MaxPooling3D
-from keras.layers.core import Activation, Dense, Dropout
+from keras.layers.core import Activation, Dense, Dropout, Reshape, Masking
 from keras.layers.advanced_activations import LeakyReLU
 
 from keras.optimizers import SGD
@@ -17,7 +17,7 @@ import pdb
 #TODO add for all parameters
 #Find proper Optimizer
 class model_vt (object):
-    
+
     def __init__(self):
         """initiate Model according to voxnet paper"""
         # Stochastic Gradient Decent (SGD) with momentum
@@ -25,18 +25,18 @@ class model_vt (object):
         # lr=0.001 for other datasets
         # decay of 0.00016667 approx the same as learning schedule (0:0.001,60000:0.0001,600000:0.00001)
         self._optimizer = SGD(lr=0.001, momentum=0.9, decay=0.00016667, nesterov=False)
-        
+
         #init model
         self._mdl= Sequential()
-
+        samples = 100
+        batch_size = 10
         #Convolution1
-        self._mdl.add(Convolution3D(input_shape=(1, 32, 32, 32),
+        self._mdl.add(Convolution3D(batch_input_shape=(samples,batch_size, 32, 32, 32),
                                     nb_filter=32,
                                     kernel_dim1=5,
                                     kernel_dim2=5,
                                     kernel_dim3=5,
                                     init='normal', #TODO
-                                    activation=LeakyReLU(alpha=0.1),
                                     weights=None, #TODO
                                     border_mode='valid',
                                     subsample=(2, 2, 2),
@@ -46,17 +46,17 @@ class model_vt (object):
                                     activity_regularizer=None,
                                     W_constraint=None,
                                     b_constraint=None))
-
+        self._mdl.add(Activation(LeakyReLU()))
         #Dropout1
         self._mdl.add(Dropout(p=0.2))
-        
+
         #Convolution2
-        self._mdl.add(Convolution3D(nb_filter=32,
+        self._mdl.add(Convolution3D(
+                                    nb_filter=32,
                                     kernel_dim1=3,
                                     kernel_dim2=3,
                                     kernel_dim3=3,
                                     init='normal', #TODO
-                                    activation=LeakyReLU(alpha=0.1),
                                     weights=None, #TODO
                                     border_mode='valid',
                                     subsample=(1, 1, 1),
@@ -66,18 +66,24 @@ class model_vt (object):
                                     activity_regularizer=None,
                                     W_constraint=None,
                                     b_constraint=None))
-
+        # TODO: Does this work as well, adding the LeakyReLU here?
+        # Otherwise following error. Exception: You tried to call layer "leakyrelu_1".
+        #This layer has no information about its expected input shape, and thus cannot be built.
+        #You can build it manually via: `layer.build(batch_input_shape)`
+        model.add(Activation(LeakyReLU()))
         #MaxPool1
         self._mdl.add(MaxPooling3D(pool_size=(2, 2, 2),
                                   strides=None,
                                   border_mode='valid',
                                   dim_ordering='th')) #TODO
-        
+
         #Dropout2
         self._mdl.add(Dropout(p=0.3))
-    
+        # Dense Input: 2D tensor with shape: (nb_samples, input_dim).
+        # TODO, is this what we want, the reshape?
+        self._mdl.add(Reshape([32*6*6*6]))
         #Dense1
-        self._mdl.add(Dense( output_dim=128, # TODO not sure ...
+        self._mdl.add(Dense(output_dim=128, # TODO not sure ...
                                init='normal',  #TODO np.random.normal, K.random_normal
                                activation='linear',
                                weights=None,
@@ -86,10 +92,10 @@ class model_vt (object):
                                activity_regularizer=None,
                                W_constraint=None,
                                b_constraint=None))
-        
+
         #Dropout3
         self._mdl.add(Dropout(p=0.4))
-        
+
         #Dense2
         self._mdl.add(Dense( output_dim=1, # TODO not sure ...
                                init='normal', #TODO np.random.normal, K.random_normal
@@ -102,6 +108,7 @@ class model_vt (object):
                                b_constraint=None))
 
         #Compile Model
+        # TODO compile
         self._mdl.compile(loss=self._objective, optimizer=self._optimizer)
 
 
