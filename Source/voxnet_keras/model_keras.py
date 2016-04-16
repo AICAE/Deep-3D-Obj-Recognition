ea6e8aba43ea8efe8812be2227bbcbffdaafe7d4
adrischn <adrischn@student.ethz.ch>
2016-04-16 10:39:31 +0200

    little weaks for the model, add self._objective (cost function)

--- Source/voxnet_keras/model_keras.py
+++ Source/voxnet_keras/model_keras.py
@@ -10,6 +10,7 @@ from keras.models import Sequential
 from keras.layers import Convolution3D, MaxPooling3D
 from keras.layers.core import Activation, Dense, Dropout, Flatten
 from keras.layers.advanced_activations import LeakyReLU
+from keras.regularizers import l2
 
 from keras.optimizers import SGD
 
@@ -28,16 +29,16 @@ class model_vt (object):
         self._optimizer = SGD(lr=0.001, momentum=0.9, decay=0.00016667, nesterov=False)
 
         # init model
-        self._mdl= Sequential()
-        
+        self._mdl = Sequential()
+
         # convolution 1
         self._mdl.add(Convolution3D(input_shape=(1, 32, 32, 32),
                                     nb_filter=32,
                                     kernel_dim1=5,
                                     kernel_dim2=5,
                                     kernel_dim3=5,
-                                    init='normal', #TODO
-                                    weights=None, #TODO
+                                    init='normal',  # TODO
+                                    weights=None,  # TODO
                                     border_mode='valid',
                                     subsample=(2, 2, 2),
                                     dim_ordering='th',
@@ -46,10 +47,10 @@ class model_vt (object):
                                     activity_regularizer=None,
                                     W_constraint=None,
                                     b_constraint=None))
-        
+
         logging.debug("Layer1:Conv3D shape={0}".format(self._mdl.output_shape))
         self._mdl.add(Activation(LeakyReLU(alpha=0.1)))
-        
+
         # dropout 1
         self._mdl.add(Dropout(p=0.2))
 
@@ -58,8 +59,8 @@ class model_vt (object):
                                     kernel_dim1=3,
                                     kernel_dim2=3,
                                     kernel_dim3=3,
-                                    init='normal', #TODO
-                                    weights=None, #TODO
+                                    init='normal',  # TODO
+                                    weights=None,  # TODO
                                     border_mode='valid',
                                     subsample=(1, 1, 1),
                                     dim_ordering='th',
@@ -68,16 +69,16 @@ class model_vt (object):
                                     activity_regularizer=None,
                                     W_constraint=None,
                                     b_constraint=None))
-        
+
         logging.debug("Layer3:Conv3D shape={0}".format(self._mdl.output_shape))
         self._mdl.add(Activation(LeakyReLU(alpha=0.1)))
-        
+
         # max pool 1
         self._mdl.add(MaxPooling3D(pool_size=(2, 2, 2),
-                                  strides=None,
-                                  border_mode='valid',
-                                  dim_ordering='th'))
-        
+                                   strides=None,
+                                   border_mode='valid',
+                                   dim_ordering='th'))
+
         logging.debug("Layer4:MaxPool3D shape={0}".format(self._mdl.output_shape))
 
         # dropout 2
@@ -86,72 +87,79 @@ class model_vt (object):
         # dense 1 (fully connected layer)
         self._mdl.add(Flatten())
         logging.debug("Layer5:Flatten shape={0}".format(self._mdl.output_shape))
-        
-        self._mdl.add(Dense(output_dim=128, # TODO not sure ...
-                               init='normal',  #TODO np.random.normal, K.random_normal
-                               activation='linear',
-                               weights=None,
-                               W_regularizer=None,
-                               b_regularizer=None,
-                               activity_regularizer=None,
-                               W_constraint=None,
-                               b_constraint=None))
-        
+
+        self._mdl.add(Dense(output_dim=128,
+                            init='normal',  # TODO np.random.normal, K.random_normal
+                            activation='linear',
+                            weights=None,
+                            W_regularizer=None,
+                            b_regularizer=None,
+                            activity_regularizer=None,
+                            W_constraint=None,
+                            b_constraint=None))
+
         logging.debug("Layer6:Dense shape={0}".format(self._mdl.output_shape))
 
         # dropout 3
         self._mdl.add(Dropout(p=0.4))
 
-        # dense 2
-        self._mdl.add(Dense( output_dim=1, # TODO not sure ...
-                               init='normal', #TODO np.random.normal, K.random_normal
-                               activation='linear',
-                               weights=None,
-                               W_regularizer=None,
-                               b_regularizer=None,
-                               activity_regularizer=None,
-                               W_constraint=None,
-                               b_constraint=None))
-        
+        # dense 2 (fully connected layer)
+        self._mdl.add(Dense(output_dim=1,
+                            init='normal',  # TODO np.random.normal, K.random_normal
+                            activation='linear',
+                            weights=None,
+                            W_regularizer=l2(0.001),
+                            b_regularizer=l2(0.001),
+                            activity_regularizer=None,
+                            W_constraint=None,
+                            b_constraint=None))
+
         logging.debug("Layer8:Dense shape={0}".format(self._mdl.output_shape))
+
+        # TODO softmax needed ?
+        self._mdl.add(Activation("softmax"))
 
         # compile model
         self._mdl.compile(loss=self._objective, optimizer=self._optimizer)
-
+        logging.debug("Model compiled!")
 
     def _objective(self, y_true, y_pred):
-        #TODO replace!
-        return K.mean(K.square(y_pred - y_true), axis=-1)
+        # TODO might need to use np_utils.to_categorical(y, nb_classes=None)
+        return K.mean(K.categorical_crossentropy(y_pred, y_true), axis=-1)
 
-
-    def fit(self, X_train, y_train, cv=10):
-        #TODO add cross-validation
-        self.mdl.fit()
-
-
-    def add_data(self):
-        #TODO load old weigths
-        #TODO improve fit with old weigths
-        self.mdl.fit()
-
+    def fit(self, generator, samples_per_epoch, nb_epoch):
+        # TODO add cross-validation
+        # TODO where does the batch size come in ??? (generator?)
+        self._mdl.fit_generator(generator=generator,
+                                samples_per_epoch=samples_per_epoch,
+                                nb_epoch=nb_epoch,
+                                verbose=1,
+                                show_accuracy=False,
+                                callbacks=[],
+                                validation_data=None,
+                                nb_val_samples=None,
+                                class_weight=None,
+                                nb_worker=1,
+                                nb_val_worker=None)
 
     def evaluate(self, X_test, y_test):
-        #TODO make sure to use score from modelnet40/10 paper
-        self.mdl.evaluate(X_test, y_test)
+        # TODO make sure to use score from modelnet40/10 paper
+        self._score = self._mdl.evaluate(x=X_test,
+                                         y=y_test,
+                                         verbose=0)
+        print("Test score:", self.score[0])
 
-
-    def predict(self,X_predict):
-        #TODO add prediction from PointCloud
+    def predict(self, X_predict):
+        # TODO add prediction from PointCloud
         self.mdl.predict(X_predict)
 
+    def get_score(self):
+        # TODO might change to tuble(self._score), not sure it's tuple
+        return self._score
+
+    score = property(get_score)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     model_vt()
-
-
-
-
-
-
