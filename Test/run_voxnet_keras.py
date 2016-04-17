79d5a6c05087967a70714a0c2c6be9f2cb2c3678
tobiagru <tobiagru@student.ethz.ch>
2016-04-17 17:46:05 +0200

    added evaluation_generator to loader-hdf5 and added evaluation generator to model_keras.evaluation()

--- Test/run_voxnet_keras.py
+++ Test/run_voxnet_keras.py
@@ -3,12 +3,10 @@ from Source.voxnet_keras.config import model_cfg
 from Source.voxnet_keras import model_keras
 import logging
 
-logging.basicConfig(filename='debug.log', level=logging.DEBUG)
-
 voxnet = model_keras.model_vt()
 loader = lib_IO_hdf5.Loader_hdf5("/home/tg/Projects/Deep-3D-Obj-Recognition/Source/Data/testing.hdf5",
                                  set_type= "train",
-                                 batch_size= 12,
+                                 batch_size= 6,
                                  shuffle=True,
                                  valid_split=0.15,
                                  mode="train")
@@ -17,6 +15,7 @@ voxnet.fit(generator=loader.train_generator(),
           nb_epoch=5,
           valid_generator= loader.valid_generator(),
           nb_valid_samples = loader.return_num_valid_samples())
-# v.load_weights("weightsm")
-feature_valid, labels_valid = loader.return_valid_set()
-#voxnet.evaluate(feature_valid, labels_valid)
+
+
+voxnet.evaluate(evaluation_generator = loader.evaluate_generator(),
+               num_eval_samples=loader.return_num_evaluation_samples())
