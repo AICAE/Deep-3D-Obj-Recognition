8b73f652dd75bdf3c0d5a9fec03380ce32a86652
tobiagru <tobiagru@student.ethz.ch>
2016-04-18 23:59:06 +0200

    Changed Test scripts to work with loader_hdf5_test class

---
+++ src/run_voxnet_keras.py
@@ -0,0 +1,54 @@
+import lib_IO_hdf5_test
+from config import model_cfg
+import model_keras
+import logging
+import numpy as np
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+voxnet = model_keras.model_vt()
+loader = lib_IO_hdf5_test.Loader_hdf5("data/testing.hdf5",
+                                 batch_size= 12,
+                                 shuffle=True,
+                                 valid_split=0.15,
+                                 mode="train")
+voxnet.fit(generator=loader.train_generator(),
+          samples_per_epoch=loader.return_num_train_samples(),
+          nb_epoch=2,
+          valid_generator= loader.valid_generator(),
+          nb_valid_samples = loader.return_num_valid_samples())
+
+
+voxnet.evaluate(evaluation_generator = loader.evaluate_generator(),
+               num_eval_samples=loader.return_num_evaluation_samples())
+
+# def gen():
+#     while 1:
+#         feat = np.random.randint(0,1,[12,1,32,32,32])
+#         lab = np.random.randint(1,3,[12,])
+#         yield feat, lab
+#
+# def valid_gen():
+#     while 1:
+#         feat = np.random.randint(0,1,[12,1,32,32,32])
+#         lab = np.random.randint(1,3,[12,])
+#         yield feat, lab
+#
+# def eval_gen():
+#     while 1:
+#         feat = np.random.randint(0,1,[12,1,32,32,32])
+#         lab = np.random.randint(4,5,[12,])
+#         yield feat, lab
+#
+#
+# voxnet = model_keras.model_vt()
+# voxnet.fit(generator=gen(),
+#           samples_per_epoch=12 * 10,
+#           nb_epoch=4,
+#           valid_generator= valid_gen(),
+#           nb_valid_samples = 12 * 3)
+#
+#
+# voxnet.evaluate(evaluation_generator = eval_gen(),
+#                num_eval_samples= 12 * 4)
--- src/test_IO.py
+++ src/test_IO.py
@@ -1,14 +1,14 @@
 import h5py
 
-from Source.voxnet_keras import lib_IO_hdf5
-from Source.voxnet_keras.config import model_cfg
+import lib_IO_hdf5_test
+from config import model_cfg
 
 ##############test-saving####################################################
 #
 #
 # lib_IO_hdf5.save_dataset_as_hdf5("/home/tg/Downloads/volumetric_data/",
 #                            "/home/tg/Projects/Deep-3D-Obj-Recognition/Source/Data/modelnet10.hdf5",
-#                            model_cfg.class_name_to_id_modelnet10)
+#                           model_cfg.class_name_to_id_modelnet10)
 
 #################test_loading##########################################
 
@@ -37,14 +37,41 @@ from Source.voxnet_keras.config import model_cfg
 #     print("for the {0}. batch the shape is {1}".format(it,feat.shape))
 #     it +=1
 #
-b = lib_IO_hdf5.Loader_hdf5("/home/tg/Projects/Deep-3D-Obj-Recognition/Source/Data/modelnet10.hdf5",
-                                 set_type= "train",
+b = lib_IO_hdf5_test.Loader_hdf5("data/testing.hdf5",
                                  batch_size= 12,
                                  num_batches=20,
                                  shuffle=True,
                                  valid_split=0.15)
 
+print("-----------")
 it = 0
-for feat, lab in b:
-    print("for the {0}. batch the shape is {1}".format(it,feat.shape))
-    it +=1
+for feat, label in b.train_generator():
+    print(feat.shape)
+    print(label.shape)
+    it += 1
+    if it >= 3:
+        break
+
+print("-----------")
+it = 0
+for feat, label in b.valid_generator():
+    print(feat.shape)
+    print(label.shape)
+    it += 1
+    if it >= 3:
+        break
+
+print("-----------")
+it = 0
+for feat, label in b.evaluate_generator():
+    print(feat.shape)
+    print(label.shape)
+    it += 1
+    if it >= 3:
+        break
+
+print("-----------")
+for it in range(0,121,12):
+    print(b._info[sorted(b._pos_train_indizes[it*12:12*(it+1)]),1])
+
+
